c201709d89e3ce08a0ee3dc57710dcdff5079551
viach <gritcaev.vv@gmail.com>
2025-09-16 12:57:22 +0300

    docker

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME", "habit_db"),
         "USER": os.getenv("DB_USER", "habit_user"),
         "PASSWORD": os.getenv("DB_PASSWORD", "habit_pass"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "HOST": os.getenv("DB_HOST", "db"),
         "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
